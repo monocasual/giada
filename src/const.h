b402da9e7f896244c3c4c4e19e3493f2aeafef5f
monocasual <monocasual@gmail.com>
2013-04-26 11:24:09 +0200

    set version to 0.6.4

--- src/const.h
+++ src/const.h
@@ -32,9 +32,9 @@
 
 
 /* -- version ------------------------------------------------------- */
-#define VERSIONE 				"0.6.3"
+#define VERSIONE 				"0.6.4"
 #define VERSIONE_STR 		"Giada"
-#define VERSIONE_FLOAT	0.63f
+#define VERSIONE_FLOAT	0.64f
 
 #define CONF_FILENAME		"giada.conf"
 
