6a178bf9277a5ced746c5be8bb6d0b822dcb167d
monocasual <monocasual@gmail.com>
2013-05-09 21:51:37 +0200

    set version 0.7.0

--- src/const.h
+++ src/const.h
@@ -32,9 +32,9 @@
 
 
 /* -- version ------------------------------------------------------- */
-#define VERSIONE 				"0.6.4"
+#define VERSIONE 				"0.7.0"
 #define VERSIONE_STR 		"Giada"
-#define VERSIONE_FLOAT	0.64f
+#define VERSIONE_FLOAT	0.7f
 
 #define CONF_FILENAME		"giada.conf"
 
