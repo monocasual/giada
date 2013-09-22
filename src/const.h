b5f59b9a135faf07beb9fe31733b593db4337ec9
monocasual <info@giadamusic.com>
2013-09-22 09:50:06 +0200

    set version number to 0.8.0

--- src/const.h
+++ src/const.h
@@ -32,9 +32,9 @@
 
 
 /* -- version ------------------------------------------------------- */
-#define VERSIONE 				"0.7.3"
+#define VERSIONE 				"0.8.0"
 #define VERSIONE_STR 		"Giada"
-#define VERSIONE_FLOAT	0.73f
+#define VERSIONE_FLOAT	0.8f
 
 #define CONF_FILENAME		"giada.conf"
 
