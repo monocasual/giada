ce88b2d7813e79e0633acf0572def5d08310366f
monocasual <info@giadamusic.com>
2014-03-27 23:25:13 +0100

    add log.cpp and log.h to branch

---
+++ src/log.cpp
@@ -0,0 +1,73 @@
+/* ---------------------------------------------------------------------
+ *
+ * Giada - Your Hardcore Loopmachine
+ *
+ * log
+ *
+ * ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2014 Giovanni A. Zuliani | Monocasual
+ *
+ * This file is part of Giada - Your Hardcore Loopmachine.
+ *
+ * Giada - Your Hardcore Loopmachine is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Giada - Your Hardcore Loopmachine is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Giada - Your Hardcore Loopmachine. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * ------------------------------------------------------------------ */
+
+
+#include <cstdio>
+#include <cstdarg>
+#include "log.h"
+#include "const.h"
+
+
+static FILE *f;
+static int   mode;
+
+
+int gLog_init(int m) {
+	mode = m;
+	if (mode == LOG_MODE_FILE) {
+		f = fopen("giada.log", "w");
+		if (!f)
+			return 0;
+	}
+	return 1;
+}
+
+
+/* ------------------------------------------------------------------ */
+
+
+void gLog_close() {
+	if (mode == LOG_MODE_FILE)
+		fclose(f);
+}
+
+
+/* ------------------------------------------------------------------ */
+
+
+void gLog(const char *format, ...) {
+	if (mode == LOG_MODE_MUTE)
+		return;
+  va_list args;
+  va_start(args, format);
+  if (mode == LOG_MODE_FILE)
+		vfprintf(f, format, args);
+  else
+		vprintf(format, args);
+  va_end(args);
+}
---
+++ src/log.h
@@ -0,0 +1,38 @@
+/* ---------------------------------------------------------------------
+ *
+ * Giada - Your Hardcore Loopmachine
+ *
+ * log
+ *
+ * ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2014 Giovanni A. Zuliani | Monocasual
+ *
+ * This file is part of Giada - Your Hardcore Loopmachine.
+ *
+ * Giada - Your Hardcore Loopmachine is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Giada - Your Hardcore Loopmachine is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Giada - Your Hardcore Loopmachine. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * ------------------------------------------------------------------ */
+
+
+#ifndef __LOG_H__
+#define __LOG_H__
+
+
+int  gLog_init (int mode);
+void gLog_close();
+void gLog(const char *txt);
+
+#endif
