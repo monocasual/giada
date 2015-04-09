4720dce862c61dbb983ad97917307a5543d2493f
monocasual <monocasual@gmail.com>
2015-04-09 19:06:28 +0200

    Remove comments

--- src/ge_mixed.cpp
+++ src/ge_mixed.cpp
@@ -661,9 +661,6 @@ gFxButton::gFxButton(int x, int y, int w, int h, const char **imgOff, const char
 void gFxButton::draw()
 {
   gClick::draw();
-  if (full) {
-    //fl_color(COLOR_BD_0);
+  if (full)
     fl_rectf(x(), y()+16, w(), 4, COLOR_BD_0);
-    //fl_polygon(x()+w()-8, y()+h()-1, x()+w()-1, y()+h()-8, x()+w()-1, y()+h()-1);
-  }
 }
