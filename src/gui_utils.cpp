e157b6e8b6b4e9d5317e7f88d66ef4bc69699a2b
monocasual <monocasual@gmail.com>
2015-04-26 17:27:31 +0200

    Optimizations

--- src/gui_utils.cpp
+++ src/gui_utils.cpp
@@ -104,8 +104,8 @@ void gu_updateControls()
 	mainWin->inOut->setOutVol(G_Mixer.outVol);
 	mainWin->inOut->setInVol(G_Mixer.inVol);
 #ifdef WITH_VST
-	mainWin->inOut->setMasterFxOutFull(G_PluginHost.countPlugins(PluginHost::MASTER_OUT));
-	mainWin->inOut->setMasterFxInFull(G_PluginHost.countPlugins(PluginHost::MASTER_IN));
+	mainWin->inOut->setMasterFxOutFull(G_PluginHost.masterOut.size > 0);
+	mainWin->inOut->setMasterFxInFull(G_PluginHost.masterIn.size > 0);
 #endif
 
 	mainWin->timing->setMeter(G_Mixer.beats, G_Mixer.bars);
