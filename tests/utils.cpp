30f1d402528f081200233b89aa96e33169852b7a
monocasual <monocasual@gmail.com>
2015-09-05 22:36:01 +0200

    More test cases for utils functions

--- tests/utils.cpp
+++ tests/utils.cpp
@@ -33,3 +33,33 @@ TEST_CASE("Test gVector template")
   REQUIRE(v2.at(2) == 2); 
 }
 
+
+TEST_CASE("Test filesystem utils")
+{
+  REQUIRE(gFileExists("giada_tests") == true);
+  REQUIRE(gFileExists("ghost_file") == false);
+  REQUIRE(gDirExists("src/") == true);
+  REQUIRE(gDirExists("ghost_dir/") == false);
+  REQUIRE(gIsDir("src/") == true);
+  REQUIRE(gIsDir("giada_tests") == false);
+  REQUIRE(gBasename("tests/utils.cpp") == "utils.cpp");
+  REQUIRE(gDirname("tests/utils.cpp") == "tests");
+  REQUIRE(gGetExt("tests/utils.cpp") == "cpp");
+  REQUIRE(gStripExt("tests/utils.cpp") == "tests/utils");
+}
+
+
+TEST_CASE("Test string utils")
+{
+  REQUIRE(gReplace("Giada is cool", "cool", "hot") == "Giada is hot");
+  REQUIRE(gTrim("   Giada is cool       ") == "Giada is cool");
+  REQUIRE(gItoa(666) == "666");
+
+  gVector<std::string> v;
+  gSplit("Giada is cool", " ", &v);
+  REQUIRE(v.size == 3);
+  REQUIRE(v.at(0) == "Giada");
+  REQUIRE(v.at(1) == "is");
+  REQUIRE(v.at(2) == "cool");
+}
+
