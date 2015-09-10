070e3ece7d045513743c9794477201d25d48ea72
monocasual <monocasual@gmail.com>
2015-09-10 16:35:33 +0200

    Add empty tests for new JSON-based patch

---
+++ tests/patch.cpp
@@ -0,0 +1,13 @@
+#include "../src/core/patch.h"
+#include "catch.hpp"
+
+
+TEST_CASE("Test Patch class: write patch") 
+{
+  REQUIRE(1 == 1);
+  /*
+  Patch p;
+  p.write("./test-patch", "test-patch", false);*/
+}
+
+
