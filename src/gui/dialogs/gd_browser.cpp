6ee9a0e8a42eded080a033a3bb6a0c09808f439a
monocasual <monocasual@gmail.com>
2016-05-20 13:21:31 +0200

    Fix typo

--- src/gui/dialogs/gd_browser.cpp
+++ src/gui/dialogs/gd_browser.cpp
@@ -15,7 +15,7 @@
  * Public License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
  *
- * Giada - Your Hardcore Loopmacghine is distributed in the hope that it
+ * Giada - Your Hardcore Loopmachine is distributed in the hope that it
  * will be useful, but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
