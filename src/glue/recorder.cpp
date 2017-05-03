974693dab5ec73cdcfe6e879391c2c78a1400f3a
monocasual <monocasual@gmail.com>
2017-05-03 19:03:37 +0200

    Fix segfault on clear MIDI actions

--- src/glue/recorder.cpp
+++ src/glue/recorder.cpp
@@ -43,7 +43,7 @@ namespace
 void updateChannel(geChannel *gch)
 {
   gch->ch->hasActions = recorder::hasActions(gch->ch->index);
-  if (!gch->ch->hasActions)
+  if (gch->ch->type == CHANNEL_SAMPLE && !gch->ch->hasActions)
     static_cast<geSampleChannel*>(gch)->hideActionButton();
   /* TODO - set mute=false */
   gu_refreshActionEditor(); // refresh a.editor window, it could be open
