808b443bef8d9b1a208b769ba41eb4e2ac921b9c
Monocasual Laboratories <monocasual@gmail.com>
2018-02-25 12:05:56 +0100

    Fix utils test on Windows

--- tests/utils.cpp
+++ tests/utils.cpp
@@ -18,7 +18,11 @@ TEST_CASE("Test filesystem utils")
   REQUIRE(gu_dirname("tests/utils.cpp") == "tests");
   REQUIRE(gu_getExt("tests/utils.cpp") == "cpp");
   REQUIRE(gu_stripExt("tests/utils.cpp") == "tests/utils");
+#if defined(_WIN32)
   REQUIRE(gu_isRootDir("C:\\") == true);
+#else
+  REQUIRE(gu_isRootDir("/") == true);
+#endif
   REQUIRE(gu_isRootDir("/") == true);
   REQUIRE(gu_isRootDir("/path/to/something") == false);
 }
@@ -31,8 +35,7 @@ TEST_CASE("Test string utils")
   REQUIRE(gu_iToString(666) == "666");
   REQUIRE(gu_iToString(0x99AABB, true) == "99AABB");
   REQUIRE(gu_fToString(3.14159, 2) == "3.14");
-  // Catch can't handle this so far?
-  //REQUIRE(gu_format("I see %d men with %s hats", 5, "strange") == "I see 5 men with strange hats");
+  REQUIRE(gu_format("I see %d men with %s hats", 5, "strange") == "I see 5 men with strange hats");
 
   vector<std::string> v;
   gu_split("Giada is cool", " ", &v);
