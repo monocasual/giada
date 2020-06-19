3dc97d07fd6961039291000d80dbb25c36a8dfbe
Monocasual Laboratories <monocasual@gmail.com>
2020-06-19 15:09:55 +0200

    Set MACOSX_DEPLOYMENT_TARGET to 10.14

--- .travis/script.sh
+++ .travis/script.sh
@@ -2,15 +2,15 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
-	export MACOSX_DEPLOYMENT_TARGET=10.2
-	
+	export MACOSX_DEPLOYMENT_TARGET=10.14
+	make -j 2 CXXFLAGS+=-stdlib=libc++ 
 fi
 
-make -j 2
 make rename
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 	
+	make -j 2
 	xvfb-run make check -j 2
 
 else
