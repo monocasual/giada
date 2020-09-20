56cff0d62a319e3b1b2711beb4d8d2f7094eb006
Monocasual Laboratories <monocasual@gmail.com>
2020-09-20 11:27:39 +0200

    [CI] Generate macOS bundle

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -3,26 +3,75 @@
 set -e
 
 mkdir dist
+mkdir temp
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
+	echo "UPX binary"
+
 	upx --best ./build/giada
-	# TODO - collect libraries, make Bundle
-	cp ./build/giada ./dist
+
+	echo "Create temporary directory giada.app"
+
+	mkdir temp/giada.app
+	mkdir temp/giada.app/Contents
+	mkdir temp/giada.app/Contents/Resources
+	mkdir temp/giada.app/Contents/MacOS
+	echo APPLnone > temp/giada.app/Contents/PkgInfo
+
+	echo "Generate Info.plist file"
+
+	cat << EOF > temp/giada.app/Contents/Info.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist SYSTEM "file://localhost/System/Library/DTDs/PropertyList.dtd">
+<plist version="0.9">
+<dict>
+	<key>CFBundleDisplayName</key>
+	<string>giada</string>
+	<key>CFBundleName</key>
+	<string>giada</string>
+	<key>CFBundleExecutable</key>
+	<string>giada</string>
+	<key>CFBundlePackageType</key>
+	<string>APPL</string>
+	<key>CFBundleVersion</key>
+	<string>1.0</string>
+	<key>CFBundleShortVersionString</key>
+	<string>1.0</string>
+	<key>CFBundleIconFile</key>
+	<string>giada.icns</string>
+	<key>CFBundleSignature</key>
+	<string>none</string>
+</dict>
+</plist>
+EOF
+
+	echo "Copy the binary file into the .app bundle"
+
+	mv ./build/giada temp/giada.app/Contents/MacOS/
+
+	echo "Set executable permissions"
+
+	chmod 755 temp/giada.app/Contents/MacOS/giada
+
+	echo "Copy icon (.icns)"
+
+	cp extras/giada.icns temp/giada.app/Contents/Resources/
+
+	echo "Copy final bundle to dist/, zip it and clean it up"
+
+	cp -r temp/giada.app dist/
+	zip -r dist/giada-$TRAVIS_TAG-macos.zip dist/giada.app
+	rm -rf dist/giada.app
 
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 
-	# Strip binary.
+	echo "Strip binary and move it to temp/"
 
 	strip ./build/giada
+	mv ./build/giada temp/
 
-	# Create temporary dir. Binary and desktop files must go in there. This is 
-	# necessary for linuxdeploy to work correctly.
-
-	mkdir ./temp/
-	mv ./build/giada ./temp/
-
-	# Create .desktop file.
+	echo "Create .desktop file"
 
 cat << EOF > ./temp/giada.desktop
 [Desktop Entry]
@@ -38,16 +87,16 @@ Categories=AudioVideo;Audio;X-Digital_Processing;X-Jack;X-MIDI;Midi;
 Keywords=Giada;
 EOF
 
-	# Prepare logo. 
+	echo "Prepare logo"
 	
-	mv extras/giada-logo.svg ./temp/giada.svg
+	mv extras/giada-logo.svg temp/giada.svg
 
 	# Run linuxdeploy to make the AppImage, then move it to ./dist dir. 
 	# For some reasons linuxdeploy uses the commit hash in the filename, so
 	# rename it first.
 
-	./linuxdeploy-x86_64.AppImage -e ./temp/giada -d ./temp/giada.desktop -i ./temp/giada.svg --output appimage --appdir ./temp
-	mv Giada-*-x86_64.AppImage Giada-x86_64.AppImage 
-	cp Giada-x86_64.AppImage ./dist
+	./linuxdeploy-x86_64.AppImage -e temp/giada -d temp/giada.desktop -i temp/giada.svg --output appimage --appdir temp/
+	mv Giada-*-x86_64.AppImage Giada-$TRAVIS_TAG-x86_64.AppImage 
+	cp Giada-$TRAVIS_TAG-x86_64.AppImage dist/
 
 fi
--- .travis/install.sh
+++ .travis/install.sh
@@ -11,6 +11,21 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	brew install libsndfile
 	brew install upx
 
+	# Remove dynamic libraries to force static linking.
+
+	rm -rf /usr/local/lib/librtmidi*dylib
+	rm -rf /usr/local/lib/libsamplerate*dylib
+	rm -rf /usr/local/lib/libfltk*dylib
+	rm -rf /usr/local/lib/libfltk_forms*dylib
+	rm -rf /usr/local/lib/libfltk_gl*dylib
+	rm -rf /usr/local/lib/libfltk_images*dylib
+	rm -rf /usr/local/lib/libsndfile*dylib 
+	rm -rf /usr/local/lib/libFLAC*dylib
+	rm -rf /usr/local/lib/libogg*dylib
+	rm -rf /usr/local/lib/libopus*dylib
+	rm -rf /usr/local/lib/libvorbis*dylib
+	rm -rf /usr/local/lib/libvorbisenc*dylib
+
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 
 	# Install dependencies 
