a4b6ed202ac7726b24faddcfaedf6987567bc7e7
Monocasual Laboratories <monocasual@gmail.com>
2020-09-29 18:20:41 +0200

    No VST build on Travis CI

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -6,14 +6,14 @@ mkdir build
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
-	cmake -S . -B build/ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-ObjC++ -DWITH_VST=ON
+	cmake -S . -B build/ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-ObjC++
 
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 
 	if [[ $TRAVIS_TAG != '' ]]; then  # Tagged release, no tests
-		/usr/bin/cmake -S . -B build/ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-Wno-class-memaccess -DWITH_VST=ON
+		/usr/bin/cmake -S . -B build/ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-Wno-class-memaccess
 	else                              # Regular commit, run tests
-		/usr/bin/cmake -S . -B build/ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-Wno-class-memaccess -DWITH_VST=ON -DWITH_TESTS=ON
+		/usr/bin/cmake -S . -B build/ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-Wno-class-memaccess -DWITH_TESTS=ON
 	fi
 
 fi
