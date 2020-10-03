0f680a358de509983ea1fa28ab5afb2c8810b0bf
Ronan Keryell <ronan@keryell.fr>
2020-10-03 13:33:53 -0700

    Modernize log::print to handle std::string arguments
    This can save a lot of .c_str() from the code.

--- src/utils/log.h
+++ src/utils/log.h
@@ -30,21 +30,67 @@
 #ifndef G_UTILS_LOG_H
 #define G_UTILS_LOG_H
 
+#include <cstdio>
+#include <string>
+#include <type_traits>
+#include <utility>
 
-namespace giada {
-namespace u {
-namespace log 
-{
-/* init
-Initializes logger. Mode defines where to write the output: LOG_MODE_STDOUT,
-LOG_MODE_FILE and LOG_MODE_MUTE. */
+#include "utils/fs.h"
+#include "core/const.h"
 
-int init(int mode);
+namespace giada::u {
 
-void close();
+struct log {
+  static inline FILE* f;
+  static inline int   mode;
+  static inline bool  stat;
 
-void print(const char* format, ...);
-}}}  // giada::u::log::
+  /* init
+  Initializes logger. Mode defines where to write the output: LOG_MODE_STDOUT,
+  LOG_MODE_FILE and LOG_MODE_MUTE. */
+  static int init(int mode);
 
+  static void close();
+
+  // Use forwarding references && to avoid useless string copy
+  static constexpr auto string_to_c_str = [] (auto&& s) {
+    // Remove any reference and const-ness, since the function can
+    // handle l-value and r-value, const or not.
+    // Use std::remove_cvref instead, when sitching to C++20...
+    if constexpr (std::is_same_v<std::remove_const_t<std::remove_reference_t<
+                                   decltype(s)>>,
+                                 std::string>)
+      // If the argument is a std::string return an old-style C-string
+      return s.c_str();
+    else
+      // Return the argument unchanged otherwise
+      return s;
+  };
+
+  /** A variadic printf-like logging fonction
+
+      Any `std::string` argument will be automatically transformed
+      into a C-string.
+
+      Use forwarding references to avoid useless string copy. */
+  template <typename... Args>
+  static void print(const char* format, Args&&... args) {
+    if (mode == LOG_MODE_MUTE)
+      return;
+
+    if (mode == LOG_MODE_FILE && stat == true) {
+      // Replace any std::string in the arguments by its C-string
+      std::fprintf(f, format, string_to_c_str(std::forward<Args>(args))...);
+#ifdef _WIN32
+      fflush(f);
+#endif
+    }
+    else
+      std::printf(format, string_to_c_str(std::forward<Args>(args))...);
+  }
+
+};
+
+}  // giada::u
 
 #endif
