9fad40c28ec4e7ee2bf36a41cf174536e31b7d31
Tomek Szczęsny <mctom@tlen.pl>
2020-10-11 17:28:46 +0200

    Major MidiMsgFilter redesign
    Lambda filters have been removed. It is impossible to serialize them, which is a good enough reason to give up on them.
    
    Instead, a few operators have been overloaded, namely &|! for and / or / not manipulation of filters. Note that these actually create new, fully compatible MidiMsgFilters that can be used later. For example:
    const MidiMsgFilter MMF_NOTEONOFFCC = MMF_NOTEONOFF | MMF_CC;
    Believe it or not, it actually works :D
    And all that is json serializable!
    Unfortunately, this also means that manipulating filters will be seriously limited.
    
    As a bonus, a "check message" operator have been introduced, like so:
    (MMF_CC << mm)
    Returns boolean result of checking whether MidiMsg mm is a CC message. How cool is that!

--- src/core/midiMsgFilter.cpp
+++ src/core/midiMsgFilter.cpp
@@ -33,15 +33,14 @@ namespace m {
 
 //------------------------------  CONSTRUCTORS  --------------------------------
 
-MidiMsgFilter::MidiMsgFilter(const MidiMsg& mm, bool alm) { 
-	
+MidiMsgFilter::MidiMsgFilter(const MidiMsg& mm, bool alm) {
+
 	auto l = mm.getMessageLength();
-	
+
 	m_template = *mm.getMessage();
 	m_mask.resize(l, 0xFF);
 
 	m_allow_longer_msg = alm;
-	m_lambda_filter = nullptr;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -49,36 +48,54 @@ MidiMsgFilter::MidiMsgFilter(const MidiMsg& mm, bool alm) {
 MidiMsgFilter::MidiMsgFilter() {
 
 	m_allow_longer_msg = true;
-	m_lambda_filter = nullptr;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-MidiMsgFilter::MidiMsgFilter(const int& fl, bool alm,
-					std::function<bool(MidiMsg)> ef) {
+MidiMsgFilter::MidiMsgFilter(const int& fl, bool alm) {
 
 	setFilterLength(fl);
 	m_allow_longer_msg = alm;
-	m_lambda_filter = (ef ? ef : nullptr);
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-MidiMsgFilter::MidiMsgFilter(const std::vector<unsigned char>& mask, 
-		const std::vector<unsigned char>& tmpl,
-		bool alm, std::function<bool(MidiMsg)> ef) {
+MidiMsgFilter::MidiMsgFilter(const std::vector<unsigned char>& mask,
+		const std::vector<unsigned char>& tmpl, bool alm) {
 
 		m_template = tmpl;
 		m_mask = mask;
 
 	m_allow_longer_msg = alm;
-	m_lambda_filter = (ef ? ef : nullptr);
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+MidiMsgFilter::MidiMsgFilter(const MidiMsgFilter& mmf) {
+	m_template = mmf.m_template;
+	m_mask = mmf.m_mask;
+	m_allow_longer_msg = mmf.m_allow_longer_msg;
+
+	for (auto& mbo : mmf.m_bin_ops) {
+		m_bin_ops.push_back({mbo.bo, new MidiMsgFilter(*mbo.mmf)});
+	}
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+MidiMsgFilter::~MidiMsgFilter() {
+
+	// Delete underlying MidiMsgFilters that were previously created
+	for (auto& mbo : m_bin_ops) {
+		delete mbo.mmf;
+	}
 }
 
 //----------------------------  MEMBER FUNCTIONS  ------------------------------
+
 /*
-void MidiMsgFilter::setTemplateByte(unsigned int n, unsigned char b) {
-	
+void MidiMsgFilter::setTemplateByte(const int& n, const unsigned char& b) {
+
 	// If filter is shorter than necessary,
 	// it is expanded with transparent bytes
 	if (n >= m_mask.size()) 
@@ -89,13 +106,13 @@ void MidiMsgFilter::setTemplateByte(unsigned int n, unsigned char b) {
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-void MidiMsgFilter::setMaskByte(unsigned int n, unsigned char b) {
-	
+void MidiMsgFilter::setMaskByte(const int& n, const unsigned char& b) {
+
 	// If filter is shorter than necessary,
 	// it is expanded with transparent bytes
 	if (n >= m_mask.size()) 
 		MidiMsgFilter::setFilterLength(n+1);
-	
+
 	m_mask.at(n) = b;
 }
 
@@ -116,20 +133,13 @@ void MidiMsgFilter::orTemplateByte(unsigned int n, unsigned char b,
 
 void MidiMsgFilter::orMaskByte(unsigned int n, unsigned char b,
 							unsigned int shl) {
-	
+
 	// If filter is shorter than necessary,
 	// it is expanded with transparent bytes
 	if (n >= m_mask.size()) 
 		MidiMsgFilter::setFilterLength(n-1);
-	
+
 	m_mask.at(n) |= (b << shl);
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-void MidiMsgFilter::ignoreByte(unsigned int n) {
-	if (n >= m_mask.size()) return;
-	m_mask.at(n) = 0;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -148,49 +158,64 @@ void MidiMsgFilter::disallowLongerMsg() {
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+void MidiMsgFilter::ignoreByte(unsigned int n, unsigned char mask) {
+	if (n >= m_mask.size()) return;
+	m_mask.at(n) &= mask;
+
+	// Apply the same to all related filters 
+	for (auto& mbo : m_bin_ops) {
+		mbo.mmf->ignoreByte(n, mask);
+	}
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 void MidiMsgFilter::setFilterLength(const int& fl) {
 	m_template.resize(fl, 0x00);
 	m_mask.resize(fl, 0x00);
-}
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-void MidiMsgFilter::setChannel(const unsigned& ch) {
-
-	// If filter is completely empty, expand it to 1 byte
-	if (m_mask.size() < 1) setFilterLength(1);
-
-	if ((ch >=1) and (ch <=16)) {
-		m_template[0] &= 0xF0;
-		m_template[0] += ch - 1;
-		m_mask[0]     |= 0x0F;
-	}
-	else {
-		m_mask[0]     &= 0xF0;
+	// Apply the same to all related filters 
+	for (auto& mbo : m_bin_ops) {
+		mbo.mmf->setFilterLength(fl);
 	}
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-void MidiMsgFilter::dump() const{
+void MidiMsgFilter::dump(int level) const{
+
+	if (level <=1 ) u::log::print("[MMF::dump]\n");
+	std::string tabs; 
+	for (int i=0; i<level; i++) tabs+="\t";
 
 	unsigned int fl = m_mask.size();
 
-	u::log::print("[MMF::dump]\n");
-	u::log::print("\tTemplate:");
+	u::log::print("%sTemplate:", tabs.c_str());
 
 	for (unsigned int i = 0; i < fl; i++) {
 		u::log::print("%X ", m_template.at(i));
 	}
-	u::log::print("\n\tMask:");
+	u::log::print("\tMask:");
 	for (unsigned int i = 0; i < fl; i++) {
 		u::log::print("%X ", m_mask.at(i));
 	}
-	u::log::print("\n\tLonger Messages: %s",
+	u::log::print("\tLonger Messages: %s\n",
 			m_allow_longer_msg ? "Allowed" : "Disallowed");
-	u::log::print("\n\tLambda filter: %s\n",
-		(m_lambda_filter != nullptr) ? "Defined" : "Not defined");
 
+	for (auto& mbo : m_bin_ops) {
+		switch (mbo.bo) {
+			case MMF_AND:
+				u::log::print("%sAND\n", tabs.c_str());
+				mbo.mmf->dump(level+1);
+				break;
+			case MMF_OR:
+				u::log::print("%sOR\n", tabs.c_str());
+				mbo.mmf->dump(level+1);
+				break;
+			case MMF_NOT:
+				u::log::print("%sNOT\n", tabs.c_str());
+		}
+	}
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -201,31 +226,48 @@ bool MidiMsgFilter::check(const MidiMsg& mm) const {
 	unsigned int fl = m_mask.size();
 	unsigned char b;
 
+	bool output = true;
+
 	// If message is shorter than a filter, it's clearly not a match
 	if (l < fl) 
-		return false;
+		output = false;
 
 	// Message cannot be longer than a filter if allow_longer_msg isn't set
-	if (!m_allow_longer_msg && (l > fl)) 
-		return false;
+	else if (!m_allow_longer_msg && (l > fl)) 
+		output = false;
 
-	for (unsigned int i = 0; i < fl; i++) {
-		
-		b  = mm.getByte(i);
-		b ^= m_template.at(i);
-		b &= m_mask.at(i);
+	else {
+		for (unsigned int i = 0; i < fl; i++) {
 
-		// If any meaningful difference is found, return false
-		if (b != 0) 
-			return false;
+			b  = mm.getByte(i);
+			b ^= m_template.at(i);
+			b &= m_mask.at(i);
+
+			// If any meaningful difference is found, return false
+			if (b != 0) {
+				output = false;
+				break;
+			}
+		}
 	}
 
-	// All checks succeeded so far, so lambda_filter has a final word
-	// (if defined)
-	if (m_lambda_filter != nullptr)
-		return m_lambda_filter(mm);
+	// Process underlying filters that were joined in
+	// through custom logic operators
 
-	return true;
+	for (auto& mbo : m_bin_ops) {
+		switch (mbo.bo) {
+			case MMF_AND:
+				output = output && (*mbo.mmf << mm);
+				break;
+			case MMF_OR:
+				output = output || (*mbo.mmf << mm);
+				break;
+			case MMF_NOT:
+				output = !output;
+		}
+	}
+
+	return output;
 }
 
 //----------------------------  FRIEND FUNCTIONS  ------------------------------
@@ -240,7 +282,8 @@ bool check(const MidiMsg& mm, const MidiMsgFilter& mmf) {
 void to_json(nl::json& j, const MidiMsgFilter& mmf){
 	j = nl::json{{"template", mmf.m_template},
 			{"mask", mmf.m_mask},
-			{"alm", mmf.m_allow_longer_msg}};
+			{"alm", mmf.m_allow_longer_msg},
+			{"binops", mmf.m_bin_ops}};
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -249,7 +292,17 @@ void from_json(nl::json& j, MidiMsgFilter& mmf) {
 	j.at("template").get_to(mmf.m_template);
 	j.at("mask").get_to(mmf.m_mask);
 	j.at("alm").get_to(mmf.m_allow_longer_msg);
-	mmf.m_lambda_filter = nullptr;
+	j.at("binops").get_to(mmf.m_bin_ops);
+}
+
+void mbo_to_json(nl::json& j, const MidiMsgFilter::mmfBinOps& mbo) {
+	j["bo"] = mbo.bo;
+	j["mmf"] = *mbo.mmf;
+}
+
+void mbo_from_json(const nl::json& j, MidiMsgFilter::mmfBinOps& mbo) {
+	j.at("bo").get_to(mbo.bo);
+	mbo.mmf = new MidiMsgFilter(j["mmf"]);
 }
 
 }} // giada::m::
--- src/core/midiMsgFilter.h
+++ src/core/midiMsgFilter.h
@@ -28,6 +28,7 @@
 #ifndef G_MIDIMSGFILTER_H
 #define G_MIDIMSGFILTER_H
 
+#include <memory>
 #include <vector>
 #include <functional>
 #include "midiMsg.h"
@@ -44,6 +45,10 @@ namespace m {
 //
 // TODO: Knob/slider jitter filter
 
+// Binary operators enum
+enum mmfBinOper {MMF_NOT,
+		MMF_AND,
+		MMF_OR};
 
 class MidiMsgFilter
 {
@@ -57,14 +62,23 @@ class MidiMsgFilter
 	MidiMsgFilter();
 
 	// Creates a transparent filter of a given length
-	MidiMsgFilter(const int& fl, bool alm = 0,
-				std::function<bool(MidiMsg)> ef = nullptr);
+	MidiMsgFilter(const int& fl, bool alm = 0);
 
 	// Creates a filter defined by length l, and mask and tmpl strings 
 	// m_allow_longer_msg is optional
 	MidiMsgFilter(const std::vector<unsigned char>& mask, 
-			const std::vector<unsigned char>& tmpl, bool alm = 0,
-			std::function<bool(MidiMsg)> ef = nullptr);
+			const std::vector<unsigned char>& tmpl, bool alm = 0);
+
+	// Copy constructor
+	MidiMsgFilter(const MidiMsgFilter& mmf);
+
+	// Destructor
+	~MidiMsgFilter();
+
+	// Copy assignment operator (remedy for warnings)
+	MidiMsgFilter operator=(MidiMsgFilter mmf) {
+		return MidiMsgFilter(mmf);
+	}
 
 	// Filter manipulation methods
 	// Note the byte indices are zero-based
@@ -72,27 +86,24 @@ class MidiMsgFilter
 	// 'b' is binary data byte
 	// 'shl' is left-shift of input 'b', optional
 	// 'fl' is filter length
-	// 
-	// TODO: Do we really want these functions anyway?
 /*
-	void	setTemplateByte(unsigned n, unsigned char b);
-	void	setMaskByte(unsigned n, unsigned char b);
+	void	setTemplateByte(const int& n, const unsigned char& b);
+	void	setMaskByte(const int& n, const unsigned char& b);
 	void	orTemplateByte(unsigned n, unsigned char b, unsigned shl = 0);
 	void	orMaskByte(unsigned n, unsigned char b, unsigned shl = 0);
-	void	ignoreByte(unsigned n);
 	void	allowLongerMsg();
 	void	disallowLongerMsg();
 */
 
+	// Ignores a given byte, or any subset of bits in it
+	// given as a new mask byte (zeros mean "ignore")
+	// Note that you cannot use this method to "unignore" any bits.
+	void	ignoreByte(unsigned n, unsigned char mask = 0b00000000);
 	void	setFilterLength(const int& fl);
 
-	// Set filter to accept specific NoteOnOff/CC channel 
-	// Note MIDI channels have numbers 1-16
-	// Any number outside that range (like 0) allows any channel
-	void    	setChannel(const unsigned& n);
-
 	// Dumps filter structure for diagnostic purposes
-	void		dump() const;
+	// Don't touch that level parameter.
+	void		dump(int level = 1) const;
 
 	// Check a message against this filter
 	bool		check(const MidiMsg& mm) const;
@@ -104,26 +115,55 @@ class MidiMsgFilter
 	friend void		to_json(nl::json& j, const MidiMsgFilter& mmf);
 	friend void		from_json(nl::json& j, MidiMsgFilter& mmf);
 
+	// inter-MMF binary operation
+	struct mmfBinOps {
+		mmfBinOper bo;
+		MidiMsgFilter* mmf;
+	};
+
+	// Operator overloads
+	// & - logical product of filters (and)
+	// | - logical sum of filters (or)
+	// ! - inverts filter (not)
+	// << - checks MidiMsg against this filter :)
+	MidiMsgFilter operator&(const MidiMsgFilter& mmf) const {
+		MidiMsgFilter output = *this;
+		output.m_bin_ops.push_back({MMF_AND, new MidiMsgFilter(mmf)});
+		return output;
+	}
+	MidiMsgFilter operator|(const MidiMsgFilter& mmf) const {
+		MidiMsgFilter output = *this;
+		output.m_bin_ops.push_back({MMF_OR, new MidiMsgFilter(mmf)});
+		return output;
+	}
+	MidiMsgFilter operator!() const {
+		MidiMsgFilter output = *this;
+		output.m_bin_ops.push_back({MMF_NOT, nullptr});
+		return output;
+	}
+	bool operator<<(const MidiMsg& mm) const {
+		return check(mm);
+	}
+
 	private:
 
 	// m_template vector contains data to which message is compared
 	// m_mask indicates which bits are to be included in comparison
 	// m_template and m_mask must always be of equal size	
-	// m_lambda_filter lets you define a custom message filter
-	// NOTE: since std::function cannot be serialized, lambdas are 
-	// NOT stored in json files, and filters loaded from them will have
-	// no lambda filter specified. 
-	// TODO: This HAS TO be fixed 
-	// (think about serialization of DispatchTables) 
+	// m_bin_ops contains binary relations and copies of other MMFs
+	// as constructed using binary operators by a user.
 	std::vector<unsigned char>	m_template;
 	std::vector<unsigned char>	m_mask;
 	bool 				m_allow_longer_msg;
-	std::function<bool(MidiMsg)>	m_lambda_filter;
+
+	std::vector<mmfBinOps>		m_bin_ops;
+	
 };
 
 void to_json(nl::json& j, const MidiMsgFilter& mmf);
 void from_json(nl::json& j, MidiMsgFilter& mmf);
-
+void mbo_to_json(nl::json& j, const MidiMsgFilter::mmfBinOps& mbo);
+void mbo_from_json(const nl::json& j, MidiMsgFilter::mmfBinOps& mbo);
 
 //------------------- const MidiMsgFilters for typical uses -------------------	
 
@@ -133,13 +173,12 @@ const MidiMsgFilter MMF_NOTEONOFF	= MidiMsgFilter({0xE0,0,0}, {0x80,0,0});
 const MidiMsgFilter MMF_NOTEON		= MidiMsgFilter({0xF0,0,0}, {0x90,0,0});
 const MidiMsgFilter MMF_NOTEOFF		= MidiMsgFilter({0xF0,0,0}, {0x80,0,0});
 const MidiMsgFilter MMF_CC		= MidiMsgFilter({0xF0,0,0}, {0xB0,0,0});
-const MidiMsgFilter MMF_NOTEONOFFCC	= MidiMsgFilter({0xC0,0,0}, {0x80,0,0},
-	false, ([](MidiMsg mm){return ((mm.getByte(0) & 0xF0) != 0xA0);}));
+const MidiMsgFilter MMF_NOTEONOFFCC	= MMF_NOTEONOFF | MMF_CC;
+
+}} //------------------------------- giada::m:: -------------------------------
 
 
-}} // giada::m::
-
-// The following template is necessary
+// The following templates are necessary
 // so DispatchTableItem (that includes mmf) could be serialized as well.
 namespace nlohmann {
 template <>
@@ -151,6 +190,22 @@ struct adl_serializer<giada::m::MidiMsgFilter> {
 
 	static void from_json(const json& j, giada::m::MidiMsgFilter& mmf) {
 		mmf = j.get<giada::m::MidiMsgFilter>();
+	}
+
+};
+
+template <>
+struct adl_serializer<giada::m::MidiMsgFilter::mmfBinOps> {
+
+	static void to_json(json& j,
+			const giada::m::MidiMsgFilter::mmfBinOps& mbo) {
+		
+		mbo_to_json(j, mbo);
+	}
+
+	static void from_json(const json& j,
+				giada::m::MidiMsgFilter::mmfBinOps& mbo) {
+		mbo_from_json(j, mbo);
 	}
 
 };
