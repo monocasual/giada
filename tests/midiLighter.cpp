25887a129b91a8adde84840c2b99721c16e1b32c
gvnnz <monocasual@gmail.com>
2021-10-31 14:30:24 +0100

    Add midiLighter tests

---
+++ tests/midiLighter.cpp
@@ -0,0 +1,18 @@
+#include "../src/core/channels/midiLighter.h"
+#include "mocks/kernelMidiMock.h"
+#include <catch2/catch.hpp>
+#include <memory>
+
+TEST_CASE("MidiMapper")
+{
+	using namespace giada;
+
+	m::KernelMidiMock                kernelMidi;
+	m::MidiMapper<m::KernelMidiMock> midiMapper(kernelMidi);
+	m::midiLighter::Data             midiLighter(midiMapper);
+
+	SECTION("Test initialization")
+	{
+		REQUIRE(midiLighter.enabled == false);
+	}
+}
