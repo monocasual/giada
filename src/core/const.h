46d86870ac5d5e0e1f1d2a1852472940044f13fb
gvnnz <monocasual@gmail.com>
2021-12-22 18:29:15 +0100

    Update version number to v0.19.2

--- src/core/const.h
+++ src/core/const.h
@@ -59,10 +59,10 @@
 
 /* -- version --------------------------------------------------------------- */
 constexpr auto G_APP_NAME      = "Giada";
-constexpr auto G_VERSION_STR   = "0.19.1";
+constexpr auto G_VERSION_STR   = "0.19.2";
 constexpr int  G_VERSION_MAJOR = 0;
 constexpr int  G_VERSION_MINOR = 19;
-constexpr int  G_VERSION_PATCH = 1;
+constexpr int  G_VERSION_PATCH = 2;
 
 constexpr auto CONF_FILENAME = "giada.conf";
 
