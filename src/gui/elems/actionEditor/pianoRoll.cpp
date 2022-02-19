monocasual/giada

commit 3f5c2562f4378246d5d66040fd8ba4a6bcc1c2a0

[UI] Fix Action Editor grid refresh when changing BPM while the editor window is open (fixes #547)"

src/gui/elems/actionEditor/pianoRoll.cpp
@@ -217,7 +217,7 @@ int gePianoRoll::handle(int e)
 
 void gePianoRoll::onAddAction()
 {
-	Frame frame = m_base->pixelToFrame(Fl::event_x() - x());
+	Frame frame = m_base->pixelToFrame(Fl::event_x() - x(), m_data->framesInBeat);
 	int   note  = yToNote(Fl::event_y() - y());
 	c::actionEditor::recordMidiAction(m_data->channelId, note, G_MAX_VELOCITY,
 	    frame);
@@ -298,12 +298,12 @@ void gePianoRoll::onRefreshAction()
 
 	if (!m_action->isOnEdges())
 	{
-		f1 = m_base->pixelToFrame(p1);
-		f2 = m_base->pixelToFrame(p2, /*snap=*/false) - (m_base->pixelToFrame(p1, /*snap=*/false) - f1);
+		f1 = m_base->pixelToFrame(p1, m_data->framesInBeat);
+		f2 = m_base->pixelToFrame(p2, m_data->framesInBeat, /*snap=*/false) - (m_base->pixelToFrame(p1, /*snap=*/false) - f1);
 	}
 	else if (m_action->onLeftEdge)
 	{
-		f1 = m_base->pixelToFrame(p1);
+		f1 = m_base->pixelToFrame(p1, m_data->framesInBeat);
 		f2 = m_action->a2.frame;
 		if (f1 == f2) // If snapping makes an action fall onto the other
 			f1 -= G_DEFAULT_ACTION_SIZE;
@@ -311,7 +311,7 @@ void gePianoRoll::onRefreshAction()
 	else if (m_action->onRightEdge)
 	{
 		f1 = m_action->a1.frame;
-		f2 = m_base->pixelToFrame(p2);
+		f2 = m_base->pixelToFrame(p2, m_data->framesInBeat);
 		if (f1 == f2) // If snapping makes an action fall onto the other
 			f2 += G_DEFAULT_ACTION_SIZE;
 	}

src/glue/layout.cpp
@@ -162,8 +162,7 @@ void openMidiChannelMidiOutputWindow(ID channelId)
 void openSampleActionEditor(ID channelId)
 {
 	g_ui.openSubWindow(*g_ui.mainWindow.get(),
-	    new v::gdSampleActionEditor(channelId, g_engine.conf.data, g_engine.sequencer.getFramesInBeat()),
-	    WID_ACTION_EDITOR);
+	    new v::gdSampleActionEditor(channelId, g_engine.conf.data), WID_ACTION_EDITOR);
 }
 
 /* -------------------------------------------------------------------------- */
@@ -171,8 +170,7 @@ void openSampleActionEditor(ID channelId)
 void openMidiActionEditor(ID channelId)
 {
 	g_ui.openSubWindow(*g_ui.mainWindow.get(),
-	    new v::gdMidiActionEditor(channelId, g_engine.conf.data, g_engine.sequencer.getFramesInBeat()),
-	    WID_ACTION_EDITOR);
+	    new v::gdMidiActionEditor(channelId, g_engine.conf.data), WID_ACTION_EDITOR);
 }
 
 /* -------------------------------------------------------------------------- */

src/gui/dialogs/actionEditor/baseActionEditor.cpp
@@ -45,10 +45,10 @@
 
 namespace giada::v
 {
-gdBaseActionEditor::gdBaseActionEditor(ID channelId, m::Conf::Data& conf, Frame framesInBeat)
+gdBaseActionEditor::gdBaseActionEditor(ID channelId, m::Conf::Data& conf)
 : gdWindow(conf.actionEditorX, conf.actionEditorY, conf.actionEditorW, conf.actionEditorH)
 , channelId(channelId)
-, gridTool(0, 0, conf, framesInBeat)
+, gridTool(0, 0, conf)
 , zoomInBtn(0, 0, G_GUI_UNIT, G_GUI_UNIT, "", zoomInOff_xpm, zoomInOn_xpm)
 , zoomOutBtn(0, 0, G_GUI_UNIT, G_GUI_UNIT, "", zoomOutOff_xpm, zoomOutOn_xpm)
 , m_barTop(0, 0, Direction::HORIZONTAL)
@@ -116,9 +116,9 @@ Pixel gdBaseActionEditor::frameToPixel(Frame f) const
 	return f / m_ratio;
 }
 
-Frame gdBaseActionEditor::pixelToFrame(Pixel p, bool snap) const
+Frame gdBaseActionEditor::pixelToFrame(Pixel p, Frame framesInBeat, bool snap) const
 {
-	return snap ? gridTool.getSnapFrame(p * m_ratio) : p * m_ratio;
+	return snap ? gridTool.getSnapFrame(p * m_ratio, framesInBeat) : p * m_ratio;
 }
 
 /* -------------------------------------------------------------------------- */

src/gui/dialogs/actionEditor/baseActionEditor.h
@@ -52,7 +52,7 @@ public:
 	void draw() override;
 
 	Pixel frameToPixel(Frame f) const;
-	Frame pixelToFrame(Pixel p, bool snap = true) const;
+	Frame pixelToFrame(Pixel p, Frame framesInBeat, bool snap = true) const;
 
 	ID channelId;
 
@@ -68,7 +68,7 @@ protected:
 	static constexpr float MAX_RATIO  = 40000.0f;
 	static constexpr float RATIO_STEP = 1.5f;
 
-	gdBaseActionEditor(ID channelId, m::Conf::Data&, Frame framesInBeat);
+	gdBaseActionEditor(ID channelId, m::Conf::Data&);
 
 	/* getMouseOverContent
 	Returns mouse x-position relative to the viewport content. */

src/gui/dialogs/actionEditor/midiActionEditor.cpp
@@ -32,8 +32,8 @@
 
 namespace giada::v
 {
-gdMidiActionEditor::gdMidiActionEditor(ID channelId, m::Conf::Data& conf, Frame framesInBeat)
-: gdBaseActionEditor(channelId, conf, framesInBeat)
+gdMidiActionEditor::gdMidiActionEditor(ID channelId, m::Conf::Data& conf)
+: gdBaseActionEditor(channelId, conf)
 , m_barPadding(0, 0, w() - 150, G_GUI_UNIT)
 , m_pianoRoll(0, 0, this)
 , m_velocityEditor(0, 0, this)

src/gui/dialogs/actionEditor/midiActionEditor.h
@@ -37,7 +37,7 @@ namespace giada::v
 class gdMidiActionEditor : public gdBaseActionEditor
 {
 public:
-	gdMidiActionEditor(ID channelId, m::Conf::Data&, Frame framesInBeat);
+	gdMidiActionEditor(ID channelId, m::Conf::Data&);
 	~gdMidiActionEditor();
 
 	void rebuild() override;

src/gui/dialogs/actionEditor/sampleActionEditor.cpp
@@ -36,8 +36,8 @@
 
 namespace giada::v
 {
-gdSampleActionEditor::gdSampleActionEditor(ID channelId, m::Conf::Data& conf, Frame framesInBeat)
-: gdBaseActionEditor(channelId, conf, framesInBeat)
+gdSampleActionEditor::gdSampleActionEditor(ID channelId, m::Conf::Data& conf)
+: gdBaseActionEditor(channelId, conf)
 , m_barPadding(0, 0, w() - 232, G_GUI_UNIT)
 , m_sampleActionEditor(0, 0, this)
 , m_envelopeEditor(0, 0, "Volume", this)

src/gui/dialogs/actionEditor/sampleActionEditor.h
@@ -38,7 +38,7 @@ namespace giada::v
 class gdSampleActionEditor : public gdBaseActionEditor
 {
 public:
-	gdSampleActionEditor(ID channelId, m::Conf::Data&, Frame framesInBeat);
+	gdSampleActionEditor(ID channelId, m::Conf::Data&);
 	~gdSampleActionEditor();
 
 	void rebuild() override;

commit 10bcd74e13ddb213ca9d161a691f0128c14b409a

Fix disappearing MIDI event when moved around in MIDI Action Editor (regression caused by 3f5c2562f4378246d5d66040fd8ba4a6bcc1c2a0)

src/gui/elems/actionEditor/pianoRoll.cpp
@@ -299,7 +299,7 @@ void gePianoRoll::onRefreshAction()
 	if (!m_action->isOnEdges())
 	{
 		f1 = m_base->pixelToFrame(p1, m_data->framesInBeat);
-		f2 = m_base->pixelToFrame(p2, m_data->framesInBeat, /*snap=*/false) - (m_base->pixelToFrame(p1, /*snap=*/false) - f1);
+		f2 = m_base->pixelToFrame(p2, m_data->framesInBeat, /*snap=*/false) - (m_base->pixelToFrame(p1, m_data->framesInBeat, /*snap=*/false) - f1);
 	}
 	else if (m_action->onLeftEdge)
 	{