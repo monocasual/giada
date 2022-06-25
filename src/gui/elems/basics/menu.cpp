e679b6d1451863f2bc8da868db68edf2d489d89e
gvnnz <monocasual@gmail.com>
2022-06-25 14:03:57 +0200

    [UI] New geMenu widget

---
+++ src/gui/elems/basics/menu.cpp
@@ -0,0 +1,71 @@
+/* -----------------------------------------------------------------------------
+ *
+ * Giada - Your Hardcore Loopmachine
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2022 Giovanni A. Zuliani | Monocasual Laboratories
+ *
+ * This file is part of Giada - Your Hardcore Loopmachine.
+ *
+ * Giada - Your Hardcore Loopmachine is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Giada - Your Hardcore Loopmachine is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Giada - Your Hardcore Loopmachine. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------- */
+
+#include "gui/elems/basics/menu.h"
+#include "core/const.h"
+#include "gui/elems/basics/boxtypes.h"
+#include <FL/Fl.H>
+#include <cstdio>
+
+namespace giada::v
+{
+geMenu::geMenu()
+: Fl_Menu_Button(Fl::event_x(), Fl::event_y(), 0, 0)
+, onSelect(nullptr)
+{
+	box(G_CUSTOM_BORDER_BOX);
+	textsize(G_GUI_FONT_SIZE_BASE);
+	textcolor(G_COLOR_LIGHT_2);
+	color(G_COLOR_GREY_2);
+}
+
+/* -------------------------------------------------------------------------- */
+
+void geMenu::callback(Fl_Widget* w, void* p) { static_cast<geMenu*>(w)->callback((ID)(intptr_t)p); }
+
+/* -------------------------------------------------------------------------- */
+
+void geMenu::callback(ID id)
+{
+	if (onSelect != nullptr)
+		onSelect(id);
+}
+
+/* -------------------------------------------------------------------------- */
+
+void geMenu::addItem(ID id, const char* text, int flags)
+{
+	add(text, /*shortcut=*/0, callback, /*data=*/(void*)(intptr_t)(id), flags);
+}
+
+/* -------------------------------------------------------------------------- */
+
+void geMenu::setEnabled(ID id, bool value)
+{
+	Fl_Menu_Item& item = const_cast<Fl_Menu_Item&>(menu()[(int)id]);
+	value ? item.activate() : item.deactivate();
+}
+} // namespace giada::v
---
+++ src/gui/elems/basics/menu.h
@@ -0,0 +1,54 @@
+/* -----------------------------------------------------------------------------
+ *
+ * Giada - Your Hardcore Loopmachine
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2022 Giovanni A. Zuliani | Monocasual Laboratories
+ *
+ * This file is part of Giada - Your Hardcore Loopmachine.
+ *
+ * Giada - Your Hardcore Loopmachine is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Giada - Your Hardcore Loopmachine is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Giada - Your Hardcore Loopmachine. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------- */
+
+#ifndef GE_MENU_H
+#define GE_MENU_H
+
+#include "core/types.h"
+#include <FL/Fl_Menu_.H>
+#include <FL/Fl_Menu_Button.H>
+#include <functional>
+#include <string>
+
+namespace giada::v
+{
+class geMenu : public Fl_Menu_Button
+{
+public:
+	geMenu();
+
+	void addItem(ID, const char* text, int flags = 0);
+	void setEnabled(ID, bool);
+
+	std::function<void(ID)> onSelect;
+
+private:
+	static void callback(Fl_Widget*, void*);
+	void        callback(ID);
+};
+} // namespace giada::v
+
+#endif
