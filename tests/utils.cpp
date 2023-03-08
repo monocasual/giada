97a7405c2a622b6c539f43acf2ff32156b549e79
gvnnz <monocasual@gmail.com>
2023-03-08 22:55:14 +0100

    Use more explicit namespace in tests/utils

--- tests/utils.cpp
+++ tests/utils.cpp
@@ -5,42 +5,42 @@
 
 TEST_CASE("u::fs")
 {
-	using namespace giada::u;
+	using namespace giada;
 
-	REQUIRE(fs::fileExists(TEST_RESOURCES_DIR "test.wav") == true);
-	REQUIRE(fs::fileExists("nonexistent_file") == false);
-	REQUIRE(fs::dirExists(TEST_RESOURCES_DIR) == true);
-	REQUIRE(fs::dirExists("ghost_dir/") == false);
-	REQUIRE(fs::isDir(TEST_RESOURCES_DIR) == true);
-	REQUIRE(fs::isDir("nonexistent_dir") == false);
-	REQUIRE(fs::basename("tests/utils.cpp") == "utils.cpp");
-	REQUIRE(fs::dirname("tests/utils.cpp") == "tests");
-	REQUIRE(fs::getExt("tests/utils.cpp") == ".cpp");
-	REQUIRE(fs::stripExt("tests/utils.cpp") == "tests/utils");
+	REQUIRE(u::fs::fileExists(TEST_RESOURCES_DIR "test.wav") == true);
+	REQUIRE(u::fs::fileExists("nonexistent_file") == false);
+	REQUIRE(u::fs::dirExists(TEST_RESOURCES_DIR) == true);
+	REQUIRE(u::fs::dirExists("ghost_dir/") == false);
+	REQUIRE(u::fs::isDir(TEST_RESOURCES_DIR) == true);
+	REQUIRE(u::fs::isDir("nonexistent_dir") == false);
+	REQUIRE(u::fs::basename("tests/utils.cpp") == "utils.cpp");
+	REQUIRE(u::fs::dirname("tests/utils.cpp") == "tests");
+	REQUIRE(u::fs::getExt("tests/utils.cpp") == ".cpp");
+	REQUIRE(u::fs::stripExt("tests/utils.cpp") == "tests/utils");
 #if defined(_WIN32)
-	REQUIRE(fs::isRootDir("\\") == true);
-	REQUIRE(fs::isRootDir("C:\\path\\to\\something") == false);
-	REQUIRE(fs::getUpDir("C:\\path\\to\\something") == "C:\\path\\to");
-	REQUIRE(fs::getUpDir("C:\\path") == "C:\\");
-	REQUIRE(fs::getUpDir("C:\\") == "C:\\");
+	REQUIRE(u::fs::isRootDir("\\") == true);
+	REQUIRE(u::fs::isRootDir("C:\\path\\to\\something") == false);
+	REQUIRE(u::fs::getUpDir("C:\\path\\to\\something") == "C:\\path\\to");
+	REQUIRE(u::fs::getUpDir("C:\\path") == "C:\\");
+	REQUIRE(u::fs::getUpDir("C:\\") == "C:\\");
 #else
-	REQUIRE(fs::isRootDir("/") == true);
-	REQUIRE(fs::isRootDir("/path/to/something") == false);
-	REQUIRE(fs::getUpDir("/path/to/something") == "/path/to");
-	REQUIRE(fs::getUpDir("/path") == "/");
-	REQUIRE(fs::getUpDir("/") == "/");
+	REQUIRE(u::fs::isRootDir("/") == true);
+	REQUIRE(u::fs::isRootDir("/path/to/something") == false);
+	REQUIRE(u::fs::getUpDir("/path/to/something") == "/path/to");
+	REQUIRE(u::fs::getUpDir("/path") == "/");
+	REQUIRE(u::fs::getUpDir("/") == "/");
 #endif
 }
 
 TEST_CASE("u::string")
 {
-	using namespace giada::u;
+	using namespace giada;
 
-	REQUIRE(string::replace("Giada is cool", "cool", "hot") == "Giada is hot");
-	REQUIRE(string::trim("   Giada is cool       ") == "Giada is cool");
-	REQUIRE(string::format("I see %d men with %s hats", 5, "strange") == "I see 5 men with strange hats");
+	REQUIRE(u::string::replace("Giada is cool", "cool", "hot") == "Giada is hot");
+	REQUIRE(u::string::trim("   Giada is cool       ") == "Giada is cool");
+	REQUIRE(u::string::format("I see %d men with %s hats", 5, "strange") == "I see 5 men with strange hats");
 
-	std::vector<std::string> v = string::split("Giada is cool", " ");
+	std::vector<std::string> v = u::string::split("Giada is cool", " ");
 	REQUIRE(v.size() == 3);
 	REQUIRE(v.at(0) == "Giada");
 	REQUIRE(v.at(1) == "is");
@@ -49,13 +49,13 @@ TEST_CASE("u::string")
 
 TEST_CASE("::math")
 {
-	using namespace giada::u;
+	using namespace giada;
 
-	REQUIRE(math::map(0.0f, 0.0f, 30.0f, 0.0f, 1.0f) == 0.0f);
-	REQUIRE(math::map(30.0f, 0.0f, 30.0f, 0.0f, 1.0f) == 1.0f);
-	REQUIRE(math::map(15.0f, 0.0f, 30.0f, 0.0f, 1.0f) == Approx(0.5f));
+	REQUIRE(u::math::map(0.0f, 0.0f, 30.0f, 0.0f, 1.0f) == 0.0f);
+	REQUIRE(u::math::map(30.0f, 0.0f, 30.0f, 0.0f, 1.0f) == 1.0f);
+	REQUIRE(u::math::map(15.0f, 0.0f, 30.0f, 0.0f, 1.0f) == Approx(0.5f));
 
-	REQUIRE(math::map(0.0f, 30.0f, 1.0f) == 0.0f);
-	REQUIRE(math::map(30.0f, 30.0f, 1.0f) == 1.0f);
-	REQUIRE(math::map(15.0f, 30.0f, 1.0f) == Approx(0.5f));
+	REQUIRE(u::math::map(0.0f, 30.0f, 1.0f) == 0.0f);
+	REQUIRE(u::math::map(30.0f, 30.0f, 1.0f) == 1.0f);
+	REQUIRE(u::math::map(15.0f, 30.0f, 1.0f) == Approx(0.5f));
 }
