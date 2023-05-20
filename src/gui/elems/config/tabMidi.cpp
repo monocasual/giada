1c15029c2ef370e5a9fc6d2b9878b42afe2f0a15
gvnnz <monocasual@gmail.com>
2023-05-20 21:56:50 +0200

    Config: forgot to update local viewModel data when changing MIDI map

--- src/gui/elems/config/tabMidi.cpp
+++ src/gui/elems/config/tabMidi.cpp
@@ -145,6 +145,7 @@ geTabMidi::geTabMidi(geompp::Rect<int> bounds)
 	};
 
 	m_midiMap->onChange = [this](ID id) {
+		m_data.midiMap         = id;
 		g_ui.model.midiMapPath = u::vector::atOr(m_data.midiMaps, id, "");
 	};
 
