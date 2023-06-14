39788d6addd3415a26a9af1b4fdcd0299cf6f444
gvnnz <monocasual@gmail.com>
2023-06-14 22:03:34 +0200

    Add new model::Actions class (still unused)

---
+++ src/core/model/actions.cpp
@@ -0,0 +1,293 @@
+/* -----------------------------------------------------------------------------
+ *
+ * Giada - Your Hardcore Loopmachine
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2023 Giovanni A. Zuliani | Monocasual Laboratories
+ *
+ * This file is part of Giada - Your Hardcore Loopmachine.
+ *
+ * Giada - Your Hardcore Loopmachine is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Giada - Your Hardcore Loopmachine is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Giada - Your Hardcore Loopmachine. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------- */
+
+#include "core/model/actions.h"
+#include "core/actions/actionFactory.h"
+#include "utils/log.h"
+#include <algorithm>
+#include <cassert>
+#include <memory>
+
+namespace giada::m::model
+{
+void Actions::clearAll()
+{
+	m_actions.clear();
+}
+
+/* -------------------------------------------------------------------------- */
+
+void Actions::clearChannel(ID channelId)
+{
+	removeIf([=](const Action& a) { return a.channelId == channelId; });
+}
+
+/* -------------------------------------------------------------------------- */
+
+void Actions::clearActions(ID channelId, int type)
+{
+	removeIf([=](const Action& a) {
+		return a.channelId == channelId && a.event.getStatus() == type;
+	});
+}
+
+/* -------------------------------------------------------------------------- */
+
+void Actions::deleteAction(ID id)
+{
+	removeIf([=](const Action& a) { return a.id == id; });
+}
+
+void Actions::deleteAction(ID currId, ID nextId)
+{
+	removeIf([=](const Action& a) { return a.id == currId || a.id == nextId; });
+}
+
+/* -------------------------------------------------------------------------- */
+
+void Actions::updateKeyFrames(std::function<Frame(Frame old)> f)
+{
+	Map temp;
+
+	/* Copy all existing actions in local map by cloning them, with just a
+	difference: they have a new frame value. */
+
+	for (const auto& [oldFrame, actions] : m_actions)
+	{
+		Frame newFrame = f(oldFrame);
+		for (const Action& a : actions)
+		{
+			Action copy = a;
+			copy.frame  = newFrame;
+			temp[newFrame].push_back(copy);
+		}
+		G_DEBUG("{} -> {}", oldFrame, newFrame);
+	}
+
+	updateMapPointers(temp);
+
+	m_actions = std::move(temp);
+}
+
+/* -------------------------------------------------------------------------- */
+
+void Actions::updateEvent(ID id, MidiEvent e)
+{
+	findAction(m_actions, id)->event = e;
+}
+
+/* -------------------------------------------------------------------------- */
+
+void Actions::updateSiblings(ID id, ID prevId, ID nextId)
+{
+	Action* pcurr = findAction(m_actions, id);
+	Action* pprev = findAction(m_actions, prevId);
+	Action* pnext = findAction(m_actions, nextId);
+
+	pcurr->prev   = pprev;
+	pcurr->prevId = pprev->id;
+	pcurr->next   = pnext;
+	pcurr->nextId = pnext->id;
+
+	if (pprev != nullptr)
+	{
+		pprev->next   = pcurr;
+		pprev->nextId = pcurr->id;
+	}
+	if (pnext != nullptr)
+	{
+		pnext->prev   = pcurr;
+		pnext->prevId = pcurr->id;
+	}
+}
+
+/* -------------------------------------------------------------------------- */
+
+bool Actions::hasActions(ID channelId, int type) const
+{
+	for (const auto& [frame, actions] : m_actions)
+		for (const Action& a : actions)
+			if (a.channelId == channelId && (type == 0 || type == a.event.getStatus()))
+				return true;
+	return false;
+}
+
+/* -------------------------------------------------------------------------- */
+
+Action Actions::rec(ID channelId, Frame frame, MidiEvent event)
+{
+	/* Skip duplicates. */
+
+	if (exists(channelId, frame, event))
+		return {};
+
+	Action a = actionFactory::makeAction(0, channelId, frame, event);
+
+	/* If key frame doesn't exist yet, the [] operator in std::map is smart 
+	enough to insert a new item first. No plug-in data for now. */
+
+	m_actions[frame].push_back(a);
+	updateMapPointers(m_actions);
+
+	return a;
+}
+
+/* -------------------------------------------------------------------------- */
+
+void Actions::rec(std::vector<Action>& actions)
+{
+	if (actions.size() == 0)
+		return;
+
+	for (const Action& a : actions)
+		if (!exists(a.channelId, a.frame, a.event, m_actions))
+			m_actions[a.frame].push_back(a);
+	updateMapPointers(m_actions);
+}
+
+/* -------------------------------------------------------------------------- */
+
+void Actions::rec(ID channelId, Frame f1, Frame f2, MidiEvent e1, MidiEvent e2)
+{
+	m_actions[f1].push_back(actionFactory::makeAction(0, channelId, f1, e1));
+	m_actions[f2].push_back(actionFactory::makeAction(0, channelId, f2, e2));
+
+	Action* a1 = findAction(m_actions, m_actions[f1].back().id);
+	Action* a2 = findAction(m_actions, m_actions[f2].back().id);
+	a1->nextId = a2->id;
+	a2->prevId = a1->id;
+
+	updateMapPointers(m_actions);
+}
+
+/* -------------------------------------------------------------------------- */
+
+const std::vector<Action>* Actions::getActionsOnFrame(Frame frame) const
+{
+	if (m_actions.count(frame) == 0)
+		return nullptr;
+	return &m_actions.at(frame);
+}
+
+/* -------------------------------------------------------------------------- */
+
+Action Actions::getClosestAction(ID channelId, Frame f, int type) const
+{
+	Action out = {};
+	forEachAction([&](const Action& a) {
+		if (a.event.getStatus() != type || a.channelId != channelId)
+			return;
+		if (!out.isValid() || (a.frame <= f && a.frame > out.frame))
+			out = a;
+	});
+	return out;
+}
+
+/* -------------------------------------------------------------------------- */
+
+std::vector<Action> Actions::getActionsOnChannel(ID channelId) const
+{
+	std::vector<Action> out;
+	forEachAction([&](const Action& a) {
+		if (a.channelId == channelId)
+			out.push_back(a);
+	});
+	return out;
+}
+
+/* -------------------------------------------------------------------------- */
+
+void Actions::forEachAction(std::function<void(const Action&)> f) const
+{
+	for (auto& [_, actions] : m_actions)
+		for (const Action& action : actions)
+			f(action);
+}
+
+/* -------------------------------------------------------------------------- */
+
+Action* Actions::findAction(Map& src, ID id)
+{
+	for (auto& [frame, actions] : src)
+		for (Action& a : actions)
+			if (a.id == id)
+				return &a;
+	assert(false);
+	return nullptr;
+}
+
+/* -------------------------------------------------------------------------- */
+
+void Actions::updateMapPointers(Map& src)
+{
+	for (auto& kv : src)
+	{
+		for (Action& action : kv.second)
+		{
+			if (action.nextId != 0)
+				action.next = findAction(src, action.nextId);
+			if (action.prevId != 0)
+				action.prev = findAction(src, action.prevId);
+		}
+	}
+}
+
+/* -------------------------------------------------------------------------- */
+
+void Actions::optimize(Map& map)
+{
+	for (auto it = map.cbegin(); it != map.cend();)
+		it->second.size() == 0 ? it = map.erase(it) : ++it;
+}
+
+/* -------------------------------------------------------------------------- */
+
+void Actions::removeIf(std::function<bool(const Action&)> f)
+{
+	for (auto& [frame, actions] : m_actions)
+		actions.erase(std::remove_if(actions.begin(), actions.end(), f), actions.end());
+	optimize(m_actions);
+	updateMapPointers(m_actions);
+}
+
+/* -------------------------------------------------------------------------- */
+
+bool Actions::exists(ID channelId, Frame frame, const MidiEvent& event, const Map& target) const
+{
+	for (const auto& [_, actions] : target)
+		for (const Action& a : actions)
+			if (a.channelId == channelId && a.frame == frame && a.event.getRaw() == event.getRaw())
+				return true;
+	return false;
+}
+
+/* -------------------------------------------------------------------------- */
+
+bool Actions::exists(ID channelId, Frame frame, const MidiEvent& event) const
+{
+	return exists(channelId, frame, event, m_actions);
+}
+} // namespace giada::m::model
---
+++ src/core/model/actions.h
@@ -0,0 +1,155 @@
+/* -----------------------------------------------------------------------------
+ *
+ * Giada - Your Hardcore Loopmachine
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2023 Giovanni A. Zuliani | Monocasual Laboratories
+ *
+ * This file is part of Giada - Your Hardcore Loopmachine.
+ *
+ * Giada - Your Hardcore Loopmachine is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Giada - Your Hardcore Loopmachine is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Giada - Your Hardcore Loopmachine. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------- */
+
+#ifndef G_MODEL_ACTIONS_H
+#define G_MODEL_ACTIONS_H
+
+#include "core/actions/action.h"
+#include "core/midiEvent.h"
+#include "core/types.h"
+#include <functional>
+#include <map>
+#include <memory>
+#include <vector>
+
+namespace giada::m::model
+{
+class Actions
+{
+public:
+	using Map = std::map<Frame, std::vector<Action>>;
+
+	/* forEachAction
+    Applies a read-only callback on each action recorded. NEVER do anything
+    inside the callback that might alter the ActionMap. */
+
+	void forEachAction(std::function<void(const Action&)> f) const;
+
+	/* getActionsOnChannel
+    Returns a vector of actions belonging to channel 'ch'. */
+
+	std::vector<Action> getActionsOnChannel(ID channelId) const;
+
+	/* getClosestAction
+    Given a frame 'f' returns the closest action. */
+
+	Action getClosestAction(ID channelId, Frame f, int type) const;
+
+	/* getActionsOnFrame
+    Returns a pointer to a vector of actions recorded on frame 'f', or nullptr 
+    if the frame has no actions. */
+
+	const std::vector<Action>* getActionsOnFrame(Frame f) const;
+
+	/* hasActions
+    Checks if the channel has at least one action recorded. */
+
+	bool hasActions(ID channelId, int type = 0) const;
+
+	/* clearAll
+    Deletes all recorded actions. */
+
+	void clearAll();
+
+	/* clearChannel
+    Clears all actions from a channel. */
+
+	void clearChannel(ID channelId);
+
+	/* clearActions
+    Clears the actions by type from a channel. */
+
+	void clearActions(ID channelId, int type);
+
+	/* deleteAction (1)
+    Deletes a specific action. */
+
+	void deleteAction(ID id);
+
+	/* deleteAction (2)
+    Deletes a specific pair of actions. Useful for composite stuff (i.e. MIDI). */
+
+	void deleteAction(ID currId, ID nextId);
+
+	/* updateKeyFrames
+    Update all the key frames in the internal map of actions, according to a 
+    lambda function 'f'. */
+
+	void updateKeyFrames(std::function<Frame(Frame old)> f);
+
+	/* updateEvent
+    Changes the event in action 'a'. */
+
+	void updateEvent(ID id, MidiEvent e);
+
+	/* updateSiblings
+    Changes previous and next actions in action with id 'id'. Mostly used for
+    chained actions such as envelopes. */
+
+	void updateSiblings(ID id, ID prevId, ID nextId);
+
+	/* rec (1)
+    Records an action and returns it. Used by the Action Editor. */
+
+	Action rec(ID channelId, Frame frame, MidiEvent e);
+
+	/* rec (2)
+    Transfer a vector of actions into the current ActionMap. This is called by 
+    recordHandler when a live session is over and consolidation is required. */
+
+	void rec(std::vector<Action>& actions);
+
+	/* rec (3)
+    Records two actions on channel 'channel'. Useful when recording composite 
+    actions in the Action Editor. */
+
+	void rec(ID channelId, Frame f1, Frame f2, MidiEvent e1, MidiEvent e2);
+
+private:
+	bool exists(ID channelId, Frame frame, const MidiEvent& event, const Map& target) const;
+	bool exists(ID channelId, Frame frame, const MidiEvent& event) const;
+
+	Action* findAction(Map& src, ID id);
+
+	/* updateMapPointers
+    Updates all prev/next actions pointers into the action map. This is required
+    after an action has been recorded, since pushing back new actions in a Action 
+    vector makes it reallocating the existing ones. */
+
+	void updateMapPointers(Map& src);
+
+	/* optimize
+    Removes frames without actions. */
+
+	void optimize(Map& map);
+
+	void removeIf(std::function<bool(const Action&)> f);
+
+	Actions::Map m_actions;
+};
+} // namespace giada::m::model
+
+#endif
