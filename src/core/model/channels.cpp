557a00934a20c94a7765ee0dc7ef590bcce83fbf
gvnnz <monocasual@gmail.com>
2023-07-09 15:04:58 +0200

    Print Wave pointer in model::Channels::debug()

--- src/core/model/channels.cpp
+++ src/core/model/channels.cpp
@@ -92,8 +92,10 @@ void Channels::debug() const
 
 	for (int i = 0; const Channel& c : m_channels)
 	{
-		fmt::print("\t{} - ID={} name='{}' type={} columnId={} position={} channelShared={}\n",
-		    i++, c.id, c.name, (int)c.type, c.columnId, c.position, (void*)&c.shared);
+		const Wave* wave = c.samplePlayer ? c.samplePlayer->getWave() : nullptr;
+
+		fmt::print("\t{} - ID={} name='{}' type={} columnId={} position={} channelShared={} wave={}\n",
+		    i++, c.id, c.name, (int)c.type, c.columnId, c.position, (void*)&c.shared, (void*)wave);
 
 		if (c.plugins.size() > 0)
 		{
