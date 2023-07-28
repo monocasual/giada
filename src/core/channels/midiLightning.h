b795b940167cfa225d3c895a357cf3c73ed81f7e
gvnnz <monocasual@gmail.com>
2023-07-28 17:32:29 +0200

    Add MidiLightning class (still unused)

---
+++ src/core/channels/midiLightning.h
@@ -0,0 +1,54 @@
+/* -----------------------------------------------------------------------------
+ *
+ * Giada - Your Hardcore Loopmachine
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2023 Giovanni A. Zuliani | Monocasual Laboratories
+ *
+ * This file is part of Giada - Your Hardcore Loopmachine.
+ *
+ * Giada - Your Hardcore Loopmachine is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Giada - Your Hardcore Loopmachine is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Giada - Your Hardcore Loopmachine. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------- */
+
+#ifndef G_MIDI_LIGHTNING_H
+#define G_MIDI_LIGHTNING_H
+
+#include "core/midiLearnParam.h"
+#include "core/patch.h"
+
+namespace giada::m
+{
+class MidiLightning final
+{
+public:
+	MidiLightning();
+	MidiLightning(const Patch::Channel&);
+
+	/* enabled
+    Tells whether MIDI lighting is enabled or not. */
+
+	bool enabled;
+
+	/* MIDI learning fields for MIDI lighting. */
+
+	MidiLearnParam playing;
+	MidiLearnParam mute;
+	MidiLearnParam solo;
+};
+} // namespace giada::m
+
+#endif
