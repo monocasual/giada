e096ed9732854a92e5461e13c4ec3ee1896d35e4
gvnnz <monocasual@gmail.com>
2023-11-04 22:57:21 +0100

    Adjust plug-in window size before showing it (reduce weird window flickering)

--- src/gui/dialogs/pluginWindowGUI.cpp
+++ src/gui/dialogs/pluginWindowGUI.cpp
@@ -54,9 +54,9 @@ gdPluginWindowGUI::gdPluginWindowGUI(const c::plugin::Plugin& p, ID wid)
 	/* Make sure to wait_for_expose() before showing the editor: the window must
 	be exposed and visible first. Don't fuck with multithreading! */
 
+	adjustSize();
 	show();
 	wait_for_expose();
-	adjustSize();
 	showEditor();
 
 	Fl::flush();
