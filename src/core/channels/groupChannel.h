f6059cde85c458e3f3fd2a51dff254cb6eb569e1
gvnnz <monocasual@gmail.com>
2024-03-10 23:54:36 +0100

    New GroupChannel class

---
+++ src/core/channels/groupChannel.h
@@ -0,0 +1,49 @@
+/* -----------------------------------------------------------------------------
+ *
+ * Giada - Your Hardcore Loopmachine
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2024 Giovanni A. Zuliani | Monocasual Laboratories
+ *
+ * This file is part of Giada - Your Hardcore Loopmachine.
+ *
+ * Giada - Your Hardcore Loopmachine is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Giada - Your Hardcore Loopmachine is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Giada - Your Hardcore Loopmachine. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------- */
+
+#ifndef G_GROUP_CHANNEL_H
+#define G_GROUP_CHANNEL_H
+
+#include "core/deepPtr.h"
+#include "core/patch.h"
+
+namespace giada::m::model
+{
+class Channels;
+}
+
+namespace giada::m
+{
+class GroupChannel final
+{
+public:
+	GroupChannel();
+
+	DeepPtr<model::Channels> channels; // As a pointer, due to circular deps madness
+};
+} // namespace giada::m
+
+#endif
