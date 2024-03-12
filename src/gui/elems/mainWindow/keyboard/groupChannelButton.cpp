b2f3a3d377bf667ea8737fa76a34d877b8044d40
gvnnz <monocasual@gmail.com>
2024-03-12 20:59:29 +0100

    New geGroupChannel widget

---
+++ src/gui/elems/mainWindow/keyboard/groupChannelButton.cpp
@@ -0,0 +1,46 @@
+/* -----------------------------------------------------------------------------
+ *
+ * Giada - Your Hardcore Loopmachine
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2024 Giovanni A. Zuliani | Monocasual Laboratories
+ *
+ * This file is part of Giada - Your Hardcore Loopmachine.
+ *
+ * Giada - Your Hardcore Loopmachine is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Giada - Your Hardcore Loopmachine is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Giada - Your Hardcore Loopmachine. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------- */
+
+#include "gui/elems/mainWindow/keyboard/groupChannelButton.h"
+#include "glue/channel.h"
+#include <string>
+
+namespace giada::v
+{
+geGroupChannelButton::geGroupChannelButton(const c::channel::Data& d)
+: geChannelButton(0, 0, 0, 0, d)
+{
+}
+
+/* -------------------------------------------------------------------------- */
+
+void geGroupChannelButton::refresh()
+{
+	const std::string l = m_channel.name.empty() ? "-- group --" : m_channel.name;
+	copy_label(l.c_str());
+	redraw();
+}
+} // namespace giada::v
