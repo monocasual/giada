534d27ae7499e0428b12cc4c266dedafa855876b
gvnnz <monocasual@gmail.com>
2024-03-12 20:59:29 +0100

    New geGroupChannel widget

---
+++ src/gui/elems/mainWindow/keyboard/groupChannel.h
@@ -0,0 +1,47 @@
+/* -----------------------------------------------------------------------------
+ *
+ * Giada - Your Hardcore Loopmachine
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2023 Giovanni A. Zuliani | Monocasual Laboratories
+ *
+ * This file is part of Giada - Your Hardcore Loopmachine.
+ *
+ * Giada - Your Hardcore Loopmachine is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Giada - Your Hardcore Loopmachine is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Giada - Your Hardcore Loopmachine. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------- */
+
+#ifndef GE_GROUP_CHANNEL_H
+#define GE_GROUP_CHANNEL_H
+
+#include "channel.h"
+#include "glue/channel.h"
+
+namespace giada::v
+{
+class geGroupChannel : public geChannel
+{
+public:
+	geGroupChannel(c::channel::Data d);
+
+	void resize(int x, int y, int w, int h) override;
+
+private:
+	void openMenu();
+};
+} // namespace giada::v
+
+#endif
