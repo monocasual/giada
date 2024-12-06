12627bc1ab3db16d7c880c0530ffe1ab0dd06e81
gvnnz <monocasual@gmail.com>
2024-12-06 22:10:57 +0100

    Make local variable const

--- src/core/channels/channelManager.cpp
+++ src/core/channels/channelManager.cpp
@@ -474,7 +474,7 @@ void ChannelManager::setPreviewTracker(Frame f)
 
 bool ChannelManager::saveSample(ID channelId, const std::string& filePath)
 {
-	Channel& ch = m_model.get().tracks.getChannel(channelId);
+	const Channel& ch = m_model.get().tracks.getChannel(channelId);
 
 	assert(ch.sampleChannel);
 
