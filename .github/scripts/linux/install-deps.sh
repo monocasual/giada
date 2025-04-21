e932a109657c6b6b1b08375183a8e61216d0b458
gvnnz <monocasual@gmail.com>
2025-04-21 18:41:23 +0200

    [CI, Linux] Add missing dependency needed for AppImage

--- .github/scripts/linux/install-deps.sh
+++ .github/scripts/linux/install-deps.sh
@@ -6,7 +6,7 @@ set -e
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
 sudo apt-get update -qq
-sudo apt-get install g++-10 xvfb libasound2-dev libxpm-dev libpulse-dev libjack-dev libxft-dev libxrandr-dev libx11-dev libxinerama-dev libxcursor-dev libfontconfig1-dev libgl-dev
+sudo apt-get install g++-10 xvfb libasound2-dev libxpm-dev libpulse-dev libjack-dev libxft-dev libxrandr-dev libx11-dev libxinerama-dev libxcursor-dev libfontconfig1-dev libgl-dev libfuse2
 
 # Symlink gcc in order to use the latest version
 
