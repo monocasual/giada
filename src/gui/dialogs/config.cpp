ed98352fed489de19abaa083144ebf2a6c9dd1ba
gvnnz <monocasual@gmail.com>
2025-05-10 22:33:48 +0200

    Make gdConfig minimum width a little bit larger

--- src/gui/dialogs/config.cpp
+++ src/gui/dialogs/config.cpp
@@ -47,7 +47,7 @@ namespace giada::v
 gdConfig::gdConfig(const Model& model)
 : gdWindow(u::gui::getCenterWinBounds(model.settingsBounds), g_ui->getI18Text(LangMap::CONFIG_TITLE), WID_CONFIG)
 {
-	const int minW = 400;
+	const int minW = 420;
 	const int minH = 370;
 
 	const geompp::Rect<int> bounds = getContentBounds().reduced(G_GUI_OUTER_MARGIN);
