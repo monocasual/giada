6deabc8107188a78bc798f79fb5507e87483f5eb
Giovanni Zuliani <monocasual@gmail.com>
2025-06-25 18:18:15 +0200

    [Packaging] Remove AppImage creation (#324)

--- .github/scripts/linux/install-deps.sh
+++ .github/scripts/linux/install-deps.sh
@@ -7,8 +7,3 @@ set -e
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
 sudo apt-get update -qq
 sudo apt-get install xvfb libasound2-dev libxpm-dev libpulse-dev libjack-dev libxft-dev libxrandr-dev libx11-dev libxinerama-dev libxcursor-dev libfontconfig1-dev libgl-dev libfuse2
-
-# Download linuxdeploy for building AppImages
-
-wget https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20230713-1/linuxdeploy-x86_64.AppImage
-chmod a+x linuxdeploy-x86_64.AppImage
