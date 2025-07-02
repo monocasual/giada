227f88b178b88705bdc41026fe2ada69326e64ca
Giovanni Zuliani <monocasual@gmail.com>
2025-07-02 16:17:37 +0200

    Don't use findIf in u::vector::has()
    You don't want assertions in there if the element is not found.

--- src/utils/vector.h
+++ src/utils/vector.h
@@ -66,7 +66,7 @@ auto findIf(T& v, ID id)
 template <typename T, typename F>
 bool has(const T& v, F&& func)
 {
-	return findIf(v, func) != std::cend(v);
+	return std::find_if(std::begin(v), std::end(v), func) != std::cend(v);
 }
 
 /* -------------------------------------------------------------------------- */
