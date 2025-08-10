f9e89fc97e4e9cfdc8ea146f3cbf7640d1ebde75
Giovanni Zuliani <monocasual@gmail.com>
2025-08-10 18:04:13 +0200

    UI: add Scenes widget

---
+++ src/gui/elems/mainWindow/scenes.h
@@ -0,0 +1,47 @@
+/* -----------------------------------------------------------------------------
+ *
+ * Giada - Your Hardcore Loopmachine
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2025 Giovanni A. Zuliani | Monocasual Laboratories
+ *
+ * This file is part of Giada - Your Hardcore Loopmachine.
+ *
+ * Giada - Your Hardcore Loopmachine is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Giada - Your Hardcore Loopmachine is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Giada - Your Hardcore Loopmachine. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------- */
+
+#ifndef GE_SCENES_H
+#define GE_SCENES_H
+
+#include "src/const.h"
+#include "src/gui/elems/basics/flex.h"
+#include <array>
+
+namespace giada::v
+{
+class geTextButton;
+class geScenes : public geFlex
+{
+public:
+	geScenes();
+
+private:
+	std::array<geTextButton*, G_MAX_NUM_SCENES> m_buttons;
+};
+} // namespace giada::v
+
+#endif
