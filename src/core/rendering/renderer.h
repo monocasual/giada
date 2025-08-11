685ce4d96eba066f9d71346ac375048b7c90bcbe
Giovanni Zuliani <monocasual@gmail.com>
2025-08-11 12:10:59 +0200

    Enable sample rendering with scenes (raw)

--- src/core/rendering/renderer.h
+++ src/core/rendering/renderer.h
@@ -80,13 +80,13 @@ private:
 	void advanceChannel(const Channel&, const Sequencer::EventBuffer&, SampleRange, Frame quantizerStep) const;
 
 	void renderTracks(const model::Tracks&, mcl::AudioBuffer& masterOut,
-	    mcl::AudioBuffer& hardwareOut, const mcl::AudioBuffer& in, bool hasSolos,
-	    bool seqIsRunning) const;
-	void renderNormalChannel(const Channel& ch, const mcl::AudioBuffer& in, bool seqIsRunning) const;
+	    mcl::AudioBuffer& hardwareOut, const mcl::AudioBuffer& in, std::size_t scene,
+	    bool hasSolos, bool seqIsRunning) const;
+	void renderNormalChannel(const Channel& ch, const mcl::AudioBuffer& in, std::size_t scene, bool seqIsRunning) const;
 	void renderMasterIn(const Channel&, mcl::AudioBuffer& in) const;
 	void renderMasterOut(const Channel&, mcl::AudioBuffer& out, int channelOffset) const;
 	void renderPreview(const Channel&, mcl::AudioBuffer& out) const;
-	void renderSampleChannel(const Channel&, const mcl::AudioBuffer& in, bool seqIsRunning) const;
+	void renderSampleChannel(const Channel&, const mcl::AudioBuffer& in, std::size_t scene, bool seqIsRunning) const;
 	void renderMidiChannel(const Channel&) const;
 
 	/* mergeChannel
