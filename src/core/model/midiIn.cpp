035ef1b81d2b1578f649c1b063035fe239847ed4
gvnnz <monocasual@gmail.com>
2025-12-11 16:45:21 +0100

    Add missing file

---
+++ src/core/model/midiIn.cpp
@@ -0,0 +1,56 @@
+/* -----------------------------------------------------------------------------
+ *
+ * Giada - Your Hardcore Loopmachine
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010-2025 Giovanni A. Zuliani | Monocasual Laboratories
+ *
+ * This file is part of Giada - Your Hardcore Loopmachine.
+ *
+ * Giada - Your Hardcore Loopmachine is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Giada - Your Hardcore Loopmachine is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Giada - Your Hardcore Loopmachine. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------- */
+
+#include "src/core/model/midiIn.h"
+#include "src/deps/mcl-utils/src/vector.hpp"
+
+namespace utils = mcl::utils;
+
+namespace giada::m::model
+{
+bool MidiIn::hasScene(uint32_t val) const
+{
+	return utils::vector::has(scenes, val);
+}
+
+/* -------------------------------------------------------------------------- */
+
+std::size_t MidiIn::getScene(uint32_t val) const
+{
+	assert(hasScene(val));
+
+	return utils::vector::indexOf(scenes, val);
+}
+
+/* -------------------------------------------------------------------------- */
+
+void MidiIn::setScene(std::size_t scene, uint32_t val)
+{
+	assert(scene < scenes.size());
+
+	scenes[scene] = val;
+}
+} // namespace giada::m::model
