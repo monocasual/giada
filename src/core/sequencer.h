57017e13c6661bfe1302427930bacd53cdd67202
Giovanni Zuliani <monocasual@gmail.com>
2026-05-30 11:17:04 +0200

    Sequencer: add comments

--- src/core/sequencer.h
+++ src/core/sequencer.h
@@ -68,8 +68,8 @@ public:
 	struct Event
 	{
 		EventType                  type          = EventType::NONE;
-		Frame                      global        = 0;
-		Frame                      delta         = 0;
+		Frame                      global        = 0; // Where the event occurs in the whole sequencer [0, framesInLoop)
+		Frame                      delta         = 0; // Where the event occurs in the current block [0, blockSize)
 		const std::vector<Action>* actions_DEPR_ = nullptr;
 		Scene                      scene         = {};
 	};
