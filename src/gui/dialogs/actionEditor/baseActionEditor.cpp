f66d659120f2fa26eb4950f4469bc016ff6231d4
Giovanni Zuliani <monocasual@gmail.com>
2026-06-04 12:39:53 +0200

    gdBaseActionEditor: allow 1 tick actions

--- src/gui/dialogs/actionEditor/baseActionEditor.cpp
+++ src/gui/dialogs/actionEditor/baseActionEditor.cpp
@@ -112,7 +112,7 @@ int gdBaseActionEditor::tickToPixel(Tick t) const
 TickRange gdBaseActionEditor::toTickRange(geompp::Range<int> pixelRange, bool snap) const
 {
 	const Tick t1 = pixelToTick(pixelRange.getA(), snap);
-	const Tick t2 = std::max(t1 + G_DEFAULT_ACTION_SIZE, pixelToTick(pixelRange.getB(), snap));
+	const Tick t2 = std::max(t1 + Tick{1}, pixelToTick(pixelRange.getB(), snap));
 	return {t1, t2};
 }
 
